74d281951550e62738910e6ecab25a15a0572321
Youssef Mamdouh <65975984+youssef373@users.noreply.github.com>
2023-03-16 18:43:57 +0200

    Add files via upload

---
+++ problem4/AgeCalculator/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AgeCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateAgeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DatePicker.SelectedDate == null) return;
+
+            TextBlock.Text = GetAgeString(DatePicker.SelectedDate.Value);
+        }
+
+        public string GetAgeString(DateTime dob)
+        {
+            DateTime today = DateTime.Today;
+
+            int months = today.Month - dob.Month;
+            int years = today.Year - dob.Year;
+
+            if (today.Day < dob.Day)
+            {
+                months--;
+            }
+
+            if (months < 0)
+            {
+                years--;
+                months += 12;
+            }
+
+            int days = (today - dob.AddMonths((years * 12) + months)).Days;
+
+            return $"Your current age is {years} years, {months} months and {days} days";
+        }
+    }
+}
